2256c466e75d1c9a59bf5bef6161d07c5df4ad8d
odunayo <okebunmiodunayo@gmail.com>
2020-04-30 21:42:10 +0100

    add compression middleware to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import Debug from 'debug';
+import path from 'path';
+import compression from 'compression';
+import enforce from 'express-sslify';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
@@ -9,7 +12,8 @@ import docs from './docs';
 import router from './routes';
 import { serverErrorResponse } from './helpers/serverResponse';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') dotenv.config();
+
 const debug = Debug(process.env.DEBUG);
 const app = express();
 
@@ -26,6 +30,15 @@ process.on('uncaughtException', (err) => {
   debug(err.stack);
   process.exit(1);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 const server = app.listen(process.env.PORT || 5000, () => {
   debug(`Listening on port ${server.address().port}`);
 });
